9b69c30fa48a102fc77630fc569d921d8740aa65
farnam <farnamkeshavarzian@gmail.com>
2019-11-16 15:00:02 -0800

    Added code for the memberView model and fixed the database. ReDownload DB.

--- adminpanel.cpp
+++ adminpanel.cpp
@@ -23,6 +23,23 @@ adminPanel::adminPanel(QWidget *parent) :
     {
         qDebug() << "fail!";
     }
+
+    QSqlQueryModel* modal2=new QSqlQueryModel();
+
+    QSqlQuery* qry2=new QSqlQuery();
+
+    qry2->prepare("SELECT * FROM customers ORDER by id ASC");
+
+    if(qry2->exec())
+    {
+        modal2->setQuery(*qry2);
+
+        ui->memberView->setModel(modal2);
+    }
+    else
+    {
+        qDebug() << "fail!";
+    }
 }
 
 adminPanel::~adminPanel()
